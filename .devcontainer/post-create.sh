5cfa4e2e041f95596f880a5a420814cdb87f47a0
Adam Gardner <adam.gardner@dynatrace.com>
2024-09-09 16:19:36 +1000

    add files

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+JMETER_VERSION=5.6.3
+
+# Download and extract JMeter
+wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz
+tar -xf apache-jmeter-${JMETER_VERSION}.tgz
+
+# Creation Ping
+curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"tenant\": \"$DT_URL\",
+    \"repo\": \"$GITHUB_REPOSITORY\",
+    \"demo\": \"obslab-jmeter\",
+    \"codespace.name\": \"$CODESPACE_NAME\"
+  }"
