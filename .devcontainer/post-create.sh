64c6ae78aff697765624599e01eb7e5806b4d1e0
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2025-01-20 13:47:51 +1000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -14,9 +14,9 @@ sed -i "s,DT_ENVIRONMENT_PLACEHOLDER,$DT_URL," dynatrace/monaco/manifest.yaml
 monaco deploy dynatrace/monaco/manifest.yaml
 
 # Download and extract JMeter
-wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz
-tar -xf apache-jmeter-${JMETER_VERSION}.tgz
-mv apache-jmeter-${JMETER_VERSION} apache-jmeter
+wget -O apache-jmeter.tgz https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz
+tar -xf apache-jmeter.tgz
+mv apache-jmeter-${JMETER_VERSION} /workspaces/$RepositoryName/apache-jmeter
 
 # Chop off https:// and trailing slash from DT_URL
 # Source: https://stackoverflow.com/a/16623897/9997385
@@ -25,8 +25,8 @@ suffix="/"
 DT_URL_CLEANED=${DT_URL#"$prefix"}
 DT_URL_CLEANED=${DT_URL_CLEANED%"$suffix"}
 
-sed -i "s,DT_ENVIRONMENT_PLACEHOLDER,$DT_URL_CLEANED," jmeterscripts/example.jmx
-sed -i "s,DT_API_TOKEN_PLACEHOLDER,$DT_JMETER_TOKEN," jmeterscripts/example.jmx
+sed -i "s,DT_ENVIRONMENT_PLACEHOLDER,$DT_URL_CLEANED," /workspaces/$RepositoryName/jmeterscripts/example.jmx
+sed -i "s,DT_API_TOKEN_PLACEHOLDER,$DT_JMETER_TOKEN," /workspaces/$RepositoryName/jmeterscripts/example.jmx
 
 # Creation Ping
 curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
